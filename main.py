ee655fb47e5033ee27126aba4f8a32f0723405d4
Prem Patel <80719066+sazncode@users.noreply.github.com>
2022-08-11 12:49:30 -0400

    Update main.py
    Error checking for env variables and making GSPREAD optional

--- main.py
+++ main.py
@@ -6,49 +6,58 @@ import time
 import gspread
 import json
 import base64
-import datetime
+import traceback
 import apprise
 import pandas as pd
 from spotipy.oauth2 import SpotifyOAuth
 from oauth2client.service_account import ServiceAccountCredentials
 from dotenv import load_dotenv
+
 # Load ENV
 load_dotenv()
 
-# Update with your own Spotify Credentials (Client ID, Secret Client ID, redirect, and username)
-SPOTIPY_CLIENT = os.environ['CLIENT_ID']
-SPOTIPY_SECRET_CLIENT = os.environ['SECRET_CLIENT_ID']
-SPOTIPY_REDIRECT = os.environ['REDIRECT_URL']
-SCOPE = "user-top-read playlist-modify-private playlist-modify-public user-library-modify user-library-read playlist-read-private ugc-image-upload"
-USERNAME = os.environ['USERNAME']
-
-
-# Initalize Spotipy
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT, client_secret=SPOTIPY_SECRET_CLIENT,
+# Spotify Credentials
+if not os.environ["CLIENT_ID"] or not os.environ["SECRET_CLIENT_ID"] or not os.environ["REDIRECT_URL"] or not os.environ["USERNAME"]:
+    raise Exception("Variables are missing within the .env file. Please ensure you have CLIENT_ID, SECRET_CLIENT_ID, REDIRECT_URL, and USERNAME set.")
+else:
+    # Update with your own Spotify Credentials (Client ID, Secret Client ID, redirect, and username)
+    SPOTIPY_CLIENT = os.environ['CLIENT_ID']
+    SPOTIPY_SECRET_CLIENT = os.environ['SECRET_CLIENT_ID']
+    SPOTIPY_REDIRECT = os.environ['REDIRECT_URL']
+    SCOPE = "user-top-read playlist-modify-private playlist-modify-public user-library-modify user-library-read playlist-read-private ugc-image-upload"
+    USERNAME = os.environ['USERNAME']
+    
+    # Initalize Spotipy
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT, client_secret=SPOTIPY_SECRET_CLIENT,
                      redirect_uri=SPOTIPY_REDIRECT, scope=SCOPE, username=USERNAME, open_browser=False))
 
-# gspread / Google Sheets API
-GSscope = ["https://spreadsheets.google.com/feeds",
-           "https://www.googleapis.com/auth/drive"]
-# Update with your own JSON Enviornment Variable
-my_secret = json.loads(os.environ['GSPREAD_KEYS'])
-creds = ServiceAccountCredentials.from_json_keyfile_dict(my_secret, GSscope)
-gc = gspread.authorize(creds)
-# Open exisiting Google Sheets document named Wrapped365
-sh = gc.open('Wrapped365')
+# Initialize Google Sheets, if enabled
+GOOGLE_SHEETS = os.environ.get("GSPREAD_KEYS", False)
+if GOOGLE_SHEETS:
+    # gspread / Google Sheets API
+    GSscope = ["https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive"]
+    # Update with your own JSON Enviornment Variable
+    my_secret = json.loads(os.environ['GSPREAD_KEYS'])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(my_secret, GSscope)
+    gc = gspread.authorize(creds)
+    # Open exisiting Google Sheets document named Wrapped365
+    sh = gc.open('Wrapped365')
 
 # How many seconds should the program wait until executing again
-wait = float(os.environ['MINUTES']) * 60.0
+WAIT = float(os.environ['MINUTES']) * 60.0
 
-apprise_alerts = os.environ.get("APPRISE_ALERTS").split(",")
+# Set up Apprise, if enabled
+APPRISE_ALERTS = os.environ.get("APPRISE_ALERTS")
+if APPRISE_ALERTS:
+    APPRISE_ALERTS = APPRISE_ALERTS.split(",")
 
 # Functions
-
 
 def apprise_init():
     alerts = apprise.Apprise()
     # Add all services from .env
-    for service in apprise_alerts:
+    for service in APPRISE_ALERTS:
         alerts.add(service)
     return alerts
 # Returns top artists within a time period
@@ -100,19 +109,12 @@ def insert_to_gsheet(track_ids, artist_info, time_period):
     print(time_period + ' Tracks Done.')
 
     # Insert top artists into Google Sheets
-    df = pd.DataFrame(artist_info, columns=[
-                      'name', 'spotify_url', 'artist_cover'])
+    df = pd.DataFrame(artist_info, columns=['name', 'spotify_url', 'artist_cover'])
     worksheet = sh.worksheet(f'{time_period} Artists')
     worksheet.update([df.columns.values.tolist()] + df.values.tolist())
     print(time_period + ' Artist Done.')
 
     return tracks
-
-# Wrapped-365Method
-
-
-alerts = apprise_init()
-
 
 def Wrapped():
     time_ranges = ['short_term', 'medium_term', 'long_term']
@@ -127,9 +129,9 @@ def Wrapped():
 
         # Get Top Artists
         top_artists = get_top_artists(time_period)
-
-        # Insert Top Tracks & Artists into Google Sheets
-        insert_to_gsheet(track_ids, top_artists, time_period)
+        if GOOGLE_SHEETS:
+            # Insert Top Tracks & Artists into Google Sheets
+            insert_to_gsheet(track_ids, top_artists, time_period)
 
         # get list of user's playlists & iterate over it to prevent duplication
         playlists = sp.current_user_playlists()
@@ -138,16 +140,15 @@ def Wrapped():
             if playlist['name'] == f'{period} - Top Tracks Wrapped':
                 playlist_id = playlist['id']
                 # Update songs in existing playlist
-                sp.user_playlist_replace_tracks(
-                    USERNAME, playlist_id, track_ids)
+                sp.user_playlist_replace_tracks(USERNAME, playlist_id, track_ids)
                 playlistExists = True
                 print(f'{period} playlist updated.\n')
                 break
 
         # Create playlist
-        if playlistExists == False:
+        if not playlistExists:
             playlist_id = sp.user_playlist_create(USERNAME, f'{period} - Top Tracks Wrapped', public=True, collaborative=False,
-                                                  description=f'My Top Played Tracks for {period}. Generated using SaznCode\'s Wrapped365 Python Project. Updated every {wait/60} minutes.')['id']
+                                                  description=f'My Top Played Tracks for {period}. Generated using SaznCode\'s Wrapped365 Python Project. Updated every {WAIT/60} minutes.')['id']
             sp.user_playlist_add_tracks(USERNAME, playlist_id, track_ids)
             with open(f"covers/{time_period}.jpg", 'rb') as image:
                 cover_encoded = base64.b64encode(image.read()).decode("utf-8")
@@ -156,20 +157,23 @@ def Wrapped():
 
 def main():
     try:
-        # Infinite loop
+        alerts = apprise_init()
+        
         while True:
+            if APPRISE_ALERTS:
+                alerts.notify(title=f'Wrapped365 Starting...', body='Top Artists and Tracks starting to update...')
             Wrapped()
-            try:
-                alerts.notify(title=f'Wrapped365 Finished.', body='Top Artists and Tracks Updated.')
-            except Exception as e:
-                print('Apprise Alerts Error.\n' + e)
-                continue
-            print(f'\nAll finished, sleeping for {(wait / 60)/60} hours...\n')
-            time.sleep(wait)
+            if APPRISE_ALERTS:
+                alerts.notify(title=f'Wrapped365 Finished!', body='Top Artists and Tracks Updated!')
+
+            print(f'\nAll finished, sleeping for {WAIT / 120} hours...\n')
+            time.sleep(WAIT)
+
     except Exception as e:
-        print(e)
-        alerts.notify(title=f'Wrapped365 Error.', body=f'{e}')
-        print('\nSomething went wrong, sleeping for 1 hour... before retrying\n')
+        print(f'Exception:\n{e}\n\n{traceback.format_exc()}')
+        if APPRISE_ALERTS:
+            alerts.notify(title=f'Wrapped365 Crashed.', body=f'{e}\nAttempting to restart in one hour...')
+        print(f'\Exception:\n{e}\n\n{traceback.format_exc()}\n\n')
         time.sleep(3600)
         main()
 
